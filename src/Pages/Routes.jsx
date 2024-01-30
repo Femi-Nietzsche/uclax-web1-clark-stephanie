312833ae7570dc886357bb1976b0a218c140800c
Stephanie Clark <sclark6645@gmail.com>
2024-01-29 16:25:26 -0800

    L3 complete (can't get sun and moon to appear under Course Tab but can't find errors in my code)

---
+++ src/Pages/Routes.jsx
@@ -0,0 +1,44 @@
+import { Routes, Route, HashRouter } from "react-router-dom";
+
+// global styles
+import "@/Common/css/normalize.css";
+import "@/Common/css/global.scss";
+
+// layout
+import PagesLayout from "@/Common/PagesLayout/PagesLayout.jsx";
+
+// page components
+import Home from "./Home";
+import Staff from "./Staff";
+import Contact from "./Contact";
+
+// course work page components
+import CourseWorkLayout from "./CourseWork/CourseWorkLayout.jsx";
+import SunAndMoon from "./CourseWork/SunAndMoon/SunAndMoon.jsx";
+import Essays from "./Coursework/Essays.jsx";
+import ResponsiveDesign from "./Coursework/ResponsiveDesign.jsx";
+
+const MyRoutes = () => {
+    return (
+        <HashRouter>
+            <Routes>
+                <Route element={<PagesLayout />}>
+                    <Route element={<Home />} path="" />
+                    <Route element={<Staff />} path="staff" />
+                    <Route element={<Contact />} path="contact" />
+
+                    <Route element={<CourseWorkLayout />} path="course-work">
+                        <Route elememt={<Essays />} path="" />
+                        <Route elememt={<SunAndMoon />} path="sun-and-moon" />
+                        <Route
+                            elememt={<ResponsiveDesign />}
+                            path="responsive"
+                        />
+                    </Route>
+                </Route>
+            </Routes>
+        </HashRouter>
+    );
+};
+
+export default MyRoutes;
